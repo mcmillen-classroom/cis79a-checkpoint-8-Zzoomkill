73030dacc5b1e4436c02110563bff54254325297
Zoom <oheyson980250915@gmail.com>
2019-07-21 15:49:10 -0700

    checkpoint 8. Initial commit.

---
+++ app/src/main/java/com/jiedong/myplaceapp/Place.java
@@ -0,0 +1,28 @@
+package com.jiedong.myplaceapp;
+
+public class Place
+{
+    private String mName;
+    private  String mDescription;
+
+    public Place(String name, String description) {
+        mName = name;
+        mDescription = description;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public static void setName(String name) {
+        mName = name;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+}
